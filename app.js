bc5afbc02639548f14586d7e6d0bbab79b1b82bb
clarin531 <clarinkyng@gmail.com>
2019-06-12 15:29:46 -0700

    checkpoint 12.12 revised

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function onReady(){
+function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
@@ -6,16 +6,16 @@ function onReady(){
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
 
-    //get the text
+    // get the text
     let title = newToDoText.value;
 
-    //create a new li
+    // create a new li
     let newLi = document.createElement('li');
 
-    //create a new input
+    // create a new input
     let checkbox = document.createElement('input');
 
-    //set the input's type to checkbox
+    // set the input's type to checkbox
     checkbox.type = "checkbox";
 
     //delete
@@ -23,23 +23,25 @@ function onReady(){
     deleteBTN.textContent="Delete";
     deleteBTN.addEventListener("click", function(event){
     toDoList.removeChild(this.parentElement);
-    })
-    //set the title
+    });
+
+    // set the title
     newLi.textContent = title;
 
-    //atach the checkbox to the li
+    // attach the checkbox to the li
     newLi.appendChild(checkbox);
 
-    //attach delete button
+    //attach delete btn to the li
     newLi.appendChild(deleteBTN);
-    //attach the li to the ul
+    // attach the li to the ul
     toDoList.appendChild(newLi);
 
-    //empty the input
-    newToDoText.value ="";
+   //empty the input
+   newToDoText.value = '';
+
+
   });
 };
-//completing the event handler
-window.onload = function(){
-  onReady();
-};
+ window.onload = function(){
+   onReady();
+ };
